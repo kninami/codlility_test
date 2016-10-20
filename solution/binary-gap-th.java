60b5dcf05706d11fd1a9a61064018b24d7262425
kootaehee9 <kootaehee9@naver.com>
2016-10-20 16:10:58 +0900

    Add files via upload

---
+++ solution/binary-gap-th.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int N) {
+        int N3 = 1;
+        String bina = Integer.toBinaryString(N);
+        String pro[] = bina.split("0");
+        int[] score = new int[pro.length];
+		N = 0;   
+				     
+        for(int i=1; i<pro.length ;i++){
+						if(pro[i].length()==0){
+            		N3++;
+            }else{
+            	N3=1;
+            }
+            score[0]=N3;
+            Arrays.sort(score);
+            N=score[score.length - 1]; 
+        }
+			return N;
+    }
+}
