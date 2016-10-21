6dcd3c7e2e77431f0cab2a13e5f12e4a0396602e
조우진 <woojin@tmon.co.kr>
2016-10-21 13:51:59 +0900

    add the solution of occ. problem

--- solution/binary-gap-wj.js
+++ solution/binary-gap-wj.js
@@ -4,18 +4,8 @@ node로 실행
 시간 복잡도를 O(n)으로 하기 위해 bit연산으로 하여 처리
 10진수 값을 1과 AND 연산해서 2진수의 역순으로 하나씩 꺼내서 처리
 */
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-
-rl.question('input value: ', (inputVal) => {
-  rl.close();
-
-  let curNum = inputVal;
+function solution(N) {
+  let curNum = N;
 
   let maxZeroCnt = 0;
   let curZeroCnt = 0;
@@ -41,5 +31,5 @@ rl.question('input value: ', (inputVal) => {
       break;
     }
   }
-  console.log(`max zero cnt: ${maxZeroCnt}`);
-});
+  return maxZeroCnt;
+}
